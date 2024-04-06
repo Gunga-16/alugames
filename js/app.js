e5afc5e6349e6699e046616c11894a91d9bf3ad9
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-06 18:07:05 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function alterarStatus(id){
     let gameClicado = document.getElementById(`game-${id}`);
-    let imagem = gameClicado.querySelector('dashboard__item__img');
-    let botao = gameClicado.querySelector('dashboard__item__button');
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')){
         imagem.classList.remove('dashboard__item__img--rented');
